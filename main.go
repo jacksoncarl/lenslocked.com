83c38150a6e688a91d9fb4d36566eed5c84cb435
Carl Jackson <carlanthonyjackson@googlemail.com>
2018-12-09 15:48:17 +0000

    Adding gorilla/mux router Splitting the handlerFunc into route specifc methods and adding a FAQ route and notfound.

--- main.go
+++ main.go
@@ -3,20 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email "+
-			"to <a href=\"mailto:support@lenslocked.com\">"+
-			"support@lenslocked.com</a>.")
-	}
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "To get in touch, please send an email "+
+		"to <a href=\"mailto:support@lenslocked.com\">"+
+		"support@lenslocked.com</a>.")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the FAQ page.")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Sorry this page could not be found.")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	var h http.Handler = http.HandlerFunc(notFound)
+	r := mux.NewRouter()
+	r.NotFoundHandler = h
+
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", contact)
+	http.ListenAndServe(":3000", r)
 }
